7721b2d578bbd3b2aeebf10ba11a7dcc3370e40c
halfwhole <limziwei2@gmail.com>
2021-12-28 00:12:55 +0800

    Refactor reflection ID map into separate model

---
+++ src/models/reflectionIdMap.js
@@ -0,0 +1,28 @@
+const REFLECTION_ID_MAP = {
+  1: {  // Aman 1
+    characterId: 2,
+    chapterId: 1,
+  },
+  2: {  // Nadia 1
+    characterId: 1,
+    chapterId: 1,
+  },
+  3: {  // Nadia 2
+    characterId: 1,
+    chapterId: 2,
+  },
+  4: {  // Nadia 3
+    characterId: 1,
+    chapterId: 3,
+  },
+  5: {  // Aman 2
+    characterId: 2,
+    chapterId: 2,
+  },
+  6: {  // Aman 3
+    characterId: 2,
+    chapterId: 3,
+  },
+}
+
+export default REFLECTION_ID_MAP;
