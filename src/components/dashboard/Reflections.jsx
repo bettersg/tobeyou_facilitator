1995578d1e3112ce8b70d2447eda6719f483ff96
halfwhole <limziwei2@gmail.com>
2021-12-29 22:12:51 +0800

    Add reflection modal

--- src/components/dashboard/Reflections.jsx
+++ src/components/dashboard/Reflections.jsx
@@ -1,8 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
+import { Box, Card, Modal, Typography } from '@mui/material';
 import { useAuth } from '../../contexts/AuthContext';
 import { getDbRoom } from '../../models/roomModel';
 import { getDbReflectionResponses } from '../../models/reflectionResponseModel';
+
+const ReflectionModal = (props) => {
+  const { reflectionResponses, modalReflectionIndex, setModalReflectionIndex } = props;
+  const reflection = modalReflectionIndex !== null ? reflectionResponses[modalReflectionIndex] : null;
+
+  const isLeftmostReflection = reflectionResponses && modalReflectionIndex === 0;
+  const isRightmostReflection = reflectionResponses && modalReflectionIndex === reflectionResponses.length - 1;
+
+  const handleLeft = () => {
+    if (isLeftmostReflection) return;
+    setModalReflectionIndex(modalReflectionIndex - 1);
+  }
+
+  const handleRight = () => {
+    if (isRightmostReflection) return;
+    setModalReflectionIndex(modalReflectionIndex + 1);
+  }
+
+  return (
+    <Modal
+      open={modalReflectionIndex !== null}
+      onClose={() => setModalReflectionIndex(null)}
+      aria-labelledby="reflection-modal"
+    >
+      <Box style={{ padding: 10 }}>
+        { isLeftmostReflection ? null : <KeyboardArrowLeft onClick={handleLeft}/> }
+        { isRightmostReflection ? null : <KeyboardArrowRight onClick={handleRight}/> }
+        <Typography>{ reflection ? reflection.answer : null }</Typography>
+      </Box>
+    </Modal>
+  );
+};
+
+const ReflectionCard = ({ reflectionResponse, onClick }) => {
+  const { answer } = reflectionResponse;
+  return (
+    <Card
+      variant='outlined'
+      style={{ borderRadius: 10, marginTop: 10, padding: 10, cursor: 'pointer' }}
+      onClick={onClick}
+    >
+      {answer}
+    </Card>
+  );
+};
 
 const Reflections = () => {
   const { roomId, reflectionId } = useParams();
@@ -10,6 +57,7 @@ const Reflections = () => {
   const navigate = useNavigate();
 
   const [reflectionResponses, setReflectionResponses] = useState(null);
+  const [modalReflectionIndex, setModalReflectionIndex] = useState(null);  // null means modal is inactive; an index means its reflection is displayed in the modal
 
   async function getData() {
     const dbRoom = await getDbRoom(roomId);
@@ -25,7 +73,24 @@ const Reflections = () => {
 
   return (
     <div>
-      { reflectionResponses ? reflectionResponses.map(x => <p>{x.answer}</p>) : null }
+      <KeyboardArrowLeft onClick={() => navigate(-1)}/>
+      <h2>REFLECTIONS</h2>
+      <h4>Do you have any reflections or similar stories to share after playing this chapter?</h4>
+      {
+        reflectionResponses
+          ? reflectionResponses.map((reflection, index) =>
+            <ReflectionCard
+              reflectionResponse={reflection}
+              onClick={() => { console.log(`clicking ${index}`); setModalReflectionIndex(index) } }
+            />
+          )
+          : null
+      }
+      <ReflectionModal
+        reflectionResponses={reflectionResponses}
+        modalReflectionIndex={modalReflectionIndex}
+        setModalReflectionIndex={setModalReflectionIndex}
+      />
     </div>
   );
 };
