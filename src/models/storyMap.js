e1a592f91e1e1ba22fbc655ff6fd256286818063
halfwhole <limziwei2@gmail.com>
2021-12-29 20:58:52 +0800

    Add room chapter selection

---
+++ src/models/storyMap.js
@@ -0,0 +1,21 @@
+export const CHARACTER_MAP = {
+  "Nadia": {
+    2: "Chapter 1: An Unexpected Invitation (5 min)",
+    3: "Chapter 2: The Date Crasher (6 min)",
+    4: "Chapter 3: Fork in the Road (7 min)",
+  },
+  "Aman": {
+    1: "Chapter 1: Call of Duty (6 min)",
+    5: "Chapter 2: Price of Admission (4 min)",
+    6: "Chapter 3: The Show Must Go On (5 min)",
+  }
+};
+
+export const REFLECTION_ID_MAP = {
+    1: { character: 'Aman', chapter: 1 },
+    2: { character: 'Nadia', chapter: 1 },
+    3: { character: 'Nadia', chapter: 2 },
+    4: { character: 'Nadia', chapter: 3 },
+    5: { character: 'Aman', chapter: 2 },
+    6: { character: 'Aman', chapter: 3 },
+}
