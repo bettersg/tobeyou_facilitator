24ae963e78fbb3b89a90cb5079b8a5fbf5ea4ed2
halfwhole <limziwei2@gmail.com>
2022-03-09 20:05:21 +0800

    Fix signing in with user information from db

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -6,7 +6,7 @@ const AuthContext = React.createContext(null);
 
 export const useAuth = () => {
   return useContext(AuthContext);
-}
+};
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
@@ -15,57 +15,61 @@ export const AuthProvider = ({ children }) => {
 
   const signUp = async (email, password) => {
     return await auth.createUserWithEmailAndPassword(email, password);
-  }
+  };
 
   const login = async (email, password) => {
     return await auth.signInWithEmailAndPassword(email, password);
-  }
+  };
 
   /**
    * Logs in the user only if they have already been registered as a facilitator.
    */
   const loginOnlyFacilitators = async (email, password) => {
-    // TODO: this approach is hacky and requires signing in twice. Is there a better way?
     setIsLoggingInFacilitator(true);
-    // Sign in the first time just to get the user's ID, and use it to check if they're a facilitator
-    const userCredential = await auth.signInWithEmailAndPassword(email, password);
-    const userId = userCredential.user.uid;
-    // Sign out immediately to avoid 'wrongfully' signing them in if they're not actually a facilitator
-    await auth.signOut();
-    const user = await getDbUser(userId);
+    const userCredential = await auth.signInWithEmailAndPassword(
+      email,
+      password
+    );
 
+    // Check if the user is a facilitator, and sign them out if they're not
+    // We must first sign in the user to check this, because of our firestore auth rules
+    const userId = userCredential.user.uid;
+    const user = await getDbUser(userId);
     if (!user.isFacilitator) {
-      throw new Error('The user exists, but is not registered as a facilitator yet.');
+      await auth.signOut();
+      throw new Error(
+        'The user exists, but is not registered as a facilitator yet.'
+      );
     }
 
-    // User is actually a facilitator, so sign them in proper the second time
-    await auth.signInWithEmailAndPassword(email, password);
     setIsLoggingInFacilitator(false);
     return userCredential;
-  }
+  };
 
   const logout = async () => {
     return await auth.signOut();
-  }
+  };
 
   const resetPassword = async (email) => {
     return await auth.sendPasswordResetEmail(email);
-  }
+  };
 
   const deleteUser = async () => {
     return await auth.currentUser.delete();
-  }
+  };
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      const { uid, ...rest } = user || {}
-      const nextCurrentUser = user ? {
-        ...rest,
-        id: uid,
-      } : null
+      const { uid, ...rest } = user || {};
+      const nextCurrentUser = user
+        ? {
+            ...rest,
+            id: uid,
+          }
+        : null;
       setCurrentUser(nextCurrentUser);
       setIsLoading(false);
-    })
+    });
 
     return unsubscribe;
   }, []);
