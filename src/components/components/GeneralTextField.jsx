44a0dc6ea4877d93973c124579f92fb9b8705da0
halfwhole <limziwei2@gmail.com>
2022-03-15 22:07:54 +0800

    Fix name of general text field

--- src/components/components/GeneralTextField.jsx
+++ src/components/components/GeneralTextField.jsx
@@ -1,43 +1,54 @@
-import React from "react"; 
-import { StyledTextfield } from "../styled/general";
+import React from 'react';
+import { StyledTextfield } from '../styled/general';
 import PropTypes from 'prop-types';
 import { Box, Typography } from '@mui/material';
 
-export const GeneralTextField = ({select,value, label, name, variant, placeholder, onChange, disabled, children,  ...props}) => {
-    return (
-      <Box>
-        <Typography variant="h6">{label}</Typography>
-        <StyledTextfield
-            select
-            value={value}
-            name="organisation"
-            variant="filled"
-            placeholder={placeholder}
-            onChange={onChange}
-            disabled={disabled}
-            {...props}
-        >
-          {children}
-        </StyledTextfield>
-      </Box>
-    )
-}
+export const GeneralTextField = ({
+  select,
+  value,
+  label,
+  name,
+  variant,
+  placeholder,
+  onChange,
+  disabled,
+  children,
+  ...props
+}) => {
+  return (
+    <Box>
+      <Typography variant='h6'>{label}</Typography>
+      <StyledTextfield
+        select
+        value={value}
+        name={name}
+        variant='filled'
+        placeholder={placeholder}
+        onChange={onChange}
+        disabled={disabled}
+        {...props}
+      >
+        {children}
+      </StyledTextfield>
+    </Box>
+  );
+};
 
 GeneralTextField.propTypes = {
-    label: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    variant: PropTypes.oneOf(['filled']),
-    placeholder: PropTypes.string,
-    onChange: PropTypes.func,
-    disabled: PropTypes.bool
-  };
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf(['filled']),
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
+};
 
 GeneralTextField.defaultProps = {
   // label: "Label:",
   // name: "sample text box",
   variant: 'filled',
-  select: false, 
-//   placeholder: "default text here",
+  select: false,
+  //   placeholder: "default text here",
   // onChange: undefined,
-  disabled: false
+  disabled: false,
 };
