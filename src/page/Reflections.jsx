7e4685d6268b594d1e6fe09372d9a7ada6e665bb
halfwhole <limziwei2@gmail.com>
2022-04-19 19:23:02 +0800

    Change room page

--- src/page/Reflections.jsx
+++ src/page/Reflections.jsx
@@ -5,15 +5,27 @@ import { Modal, Typography } from '@mui/material';
 import { useAuth } from '../contexts/AuthContext';
 import { getDbRoom } from '../models/roomModel';
 import { getDbReflectionResponses } from '../models/reflectionResponseModel';
-import { ModalArrowBox, ModalContentBox, ModalBox, ReflectionCard } from '../components/styled/Dashboard/reflections';
+import {
+  ModalArrowBox,
+  ModalContentBox,
+  ModalBox,
+  ReflectionCard,
+} from '../components/styled/Dashboard/reflections';
 import { Masonry } from '@mui/lab';
 
 const ReflectionModal = (props) => {
-  const { reflectionResponses, modalReflectionIndex, setModalReflectionIndex } = props;
-  const reflection = modalReflectionIndex !== null ? reflectionResponses[modalReflectionIndex] : null;
+  const { reflectionResponses, modalReflectionIndex, setModalReflectionIndex } =
+    props;
+  const reflection =
+    modalReflectionIndex !== null
+      ? reflectionResponses[modalReflectionIndex]
+      : null;
 
-  const isLeftmostReflection = reflectionResponses && modalReflectionIndex === 0;
-  const isRightmostReflection = reflectionResponses && modalReflectionIndex === reflectionResponses.length - 1;
+  const isLeftmostReflection =
+    reflectionResponses && modalReflectionIndex === 0;
+  const isRightmostReflection =
+    reflectionResponses &&
+    modalReflectionIndex === reflectionResponses.length - 1;
 
   const handleLeft = () => {
     if (isLeftmostReflection) return;
@@ -26,66 +38,73 @@ const ReflectionModal = (props) => {
   };
 
   const handleKeyDown = (event) => {
-    if (event.keyCode === 37) { // left keypress
+    if (event.keyCode === 37) {
+      // left keypress
       handleLeft();
-    } else if (event.keyCode === 39) { // right keypress
+    } else if (event.keyCode === 39) {
+      // right keypress
       handleRight();
     }
   };
 
   const LeftArrowBox = () => {
-    return isLeftmostReflection
-      ? null
-      : (
-        <ModalArrowBox>
-          <KeyboardArrowLeft onClick={handleLeft} />
-        </ModalArrowBox>
-      );
+    return isLeftmostReflection ? null : (
+      <ModalArrowBox>
+        <KeyboardArrowLeft onClick={handleLeft} />
+      </ModalArrowBox>
+    );
   };
 
   const RightArrowBox = () => {
-    return isRightmostReflection
-      ? null
-      : (
-        <ModalArrowBox>
-          <KeyboardArrowRight onClick={handleRight} />
-        </ModalArrowBox>
-      );
+    return isRightmostReflection ? null : (
+      <ModalArrowBox>
+        <KeyboardArrowRight onClick={handleRight} />
+      </ModalArrowBox>
+    );
   };
 
   return (
     <Modal
       open={modalReflectionIndex !== null}
       onClose={() => setModalReflectionIndex(null)}
-      aria-labelledby="reflection-modal"
+      aria-labelledby='reflection-modal'
       onKeyDown={handleKeyDown}
     >
       <ModalBox>
-        <LeftArrowBox/>
+        <LeftArrowBox />
         <ModalContentBox>
-          <Typography>{ reflection ? reflection.answer : null }</Typography>
+          <Typography>{reflection ? reflection.answer : null}</Typography>
         </ModalContentBox>
-        <RightArrowBox/>
+        <RightArrowBox />
       </ModalBox>
     </Modal>
   );
 };
 
 const Reflections = () => {
-  const { roomId, reflectionId } = useParams();
+  let { roomId, reflectionId } = useParams();
+  reflectionId = parseInt(reflectionId);
   const { currentUser } = useAuth();
   const navigate = useNavigate();
 
   const [reflectionResponses, setReflectionResponses] = useState(null);
-  const [modalReflectionIndex, setModalReflectionIndex] = useState(null);  // null means modal is inactive; an index means its reflection is displayed in the modal
+  const [modalReflectionIndex, setModalReflectionIndex] = useState(null); // null means modal is inactive; an index means its reflection is displayed in the modal
 
   async function getData() {
     const dbRoom = await getDbRoom(roomId);
-    if (!dbRoom || !dbRoom.facilitatorIds.includes(currentUser.id)) {
-      navigate('/');  // redirect if the room does not exist, or facilitator is unauthorised to access it
+    if (
+      !dbRoom ||
+      !dbRoom.facilitatorIds.includes(currentUser.id) ||
+      !dbRoom.reflectionIds.includes(reflectionId)
+    ) {
+      navigate('/'); // redirect if the room does not exist, or facilitator is unauthorised to access it
     }
 
-    const dbReflectionResponses = await getDbReflectionResponses(roomId, reflectionId, true);
+    const dbReflectionResponses = await getDbReflectionResponses(
+      roomId,
+      reflectionId,
+      true
+    );
     setReflectionResponses(dbReflectionResponses);
   }
 
@@ -93,13 +112,15 @@ const Reflections = () => {
 
   return (
     <div>
-      <KeyboardArrowLeft onClick={() => navigate(-1)}/>
+      <KeyboardArrowLeft onClick={() => navigate(-1)} />
       <h2>REFLECTIONS</h2>
-      <h4>Do you have any reflections or similar stories to share after playing this chapter?</h4>
+      <h4>
+        Do you have any reflections or similar stories to share after playing
+        this chapter?
+      </h4>
       <Masonry columns={3} spacing={2}>
-        {
-          reflectionResponses
-            ? reflectionResponses.map((reflectionResponse, index) =>
+        {reflectionResponses
+          ? reflectionResponses.map((reflectionResponse, index) => (
               <ReflectionCard
                 variant='outlined'
                 onClick={() => setModalReflectionIndex(index)}
@@ -107,9 +128,8 @@ const Reflections = () => {
               >
                 {reflectionResponse.answer}
               </ReflectionCard>
-            )
-            : null
-        }
+            ))
+          : null}
       </Masonry>
       <ReflectionModal
         reflectionResponses={reflectionResponses}
