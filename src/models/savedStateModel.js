1a36f017d8f09220b7ea3822a39b788575950088
halfwhole <limziwei2@gmail.com>
2022-04-26 12:24:45 +0800

    Add game choices

--- src/models/savedStateModel.js
+++ src/models/savedStateModel.js
@@ -1,18 +1,11 @@
 import { firestore } from '../firebase';
 
-// TODO: to review - might not need to filter by room code after all.
-export const getDbSavedStates = async (roomCode) => {
+export const getDbSavedState = async (id) => {
   try {
-    const snapshot = await firestore
-      .collection('savedStates')
-      .where('roomCode', '==', roomCode)
-      .get();
-    const savedStates = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    return savedStates;
+    const savedState = await firestore.collection('savedStates').doc(id).get();
+    const savedStateData = { id: savedState.id, ...savedState.data() };
+    return savedStateData;
   } catch (err) {
-    throw new Error(`Error at getDbSavedStates: ${err}`);
+    throw new Error(`Error at getDbSavedState: ${err}`);
   }
 };
