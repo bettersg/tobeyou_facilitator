8c9c8dc2a5e5c45970c33bd194892719f63c7835
halfwhole <limziwei2@gmail.com>
2022-05-04 18:45:37 +0800

    Add Ravi chapters

--- src/models/reflectionIdMap.js
+++ src/models/reflectionIdMap.js
@@ -1,28 +1,50 @@
+// TODO: refactor this with storyMap.js
 const REFLECTION_ID_MAP = {
-  1: {  // Aman 1
+  1: {
+    // Aman 1
     characterId: 2,
     chapterId: 1,
   },
-  2: {  // Nadia 1
+  2: {
+    // Nadia 1
     characterId: 1,
     chapterId: 1,
   },
-  3: {  // Nadia 2
+  3: {
+    // Nadia 2
     characterId: 1,
     chapterId: 2,
   },
-  4: {  // Nadia 3
+  4: {
+    // Nadia 3
     characterId: 1,
     chapterId: 3,
   },
-  5: {  // Aman 2
+  5: {
+    // Aman 2
     characterId: 2,
     chapterId: 2,
   },
-  6: {  // Aman 3
+  6: {
+    // Aman 3
     characterId: 2,
     chapterId: 3,
   },
-}
+  7: {
+    // Ravi 1
+    characterId: 3,
+    chapterId: 1,
+  },
+  8: {
+    // Ravi 2
+    characterId: 3,
+    chapterId: 2,
+  },
+  9: {
+    // Ravi 3
+    characterId: 3,
+    chapterId: 3,
+  },
+};
 
 export default REFLECTION_ID_MAP;
