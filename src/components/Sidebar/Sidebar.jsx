563c08b94055456f740639476f8eddf26d4524e2
halfwhole <limziwei2@gmail.com>
2022-05-05 13:10:35 +0800

    Add favicon and user avatar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { LineStyle, Logout, Circle, Person } from '@mui/icons-material';
+import { LineStyle, Logout, Person } from '@mui/icons-material';
 import {
+  Avatar,
   Box,
   Button,
   List,
@@ -14,33 +15,45 @@ import { useAuth } from '../../contexts/AuthContext';
 
 export const Sidebar = () => {
   const navigate = useNavigate();
-  const { logout } = useAuth();
+  const { currentUser, logout } = useAuth();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   return (
     <React.Fragment key={'left'}>
-      <Button onClick={() => setIsDrawerOpen(true)} sx={{color:"midnight.60"}}><Circle fontSize="large"/></Button>
+      <Button
+        onClick={() => setIsDrawerOpen(true)}
+        sx={{ color: 'midnight.60' }}
+      >
+        <Avatar
+          alt={currentUser.displayName}
+          src={currentUser.photoURL}
+          sx={{ width: 30, height: 30 }}
+        />
+      </Button>
       <SwipeableDrawer
         anchor={'right'}
         open={isDrawerOpen}
         onClose={() => setIsDrawerOpen(false)}
         onOpen={() => setIsDrawerOpen(true)}
       >
-        <Box
-          style={{ width: 250 }}
-          onClick={() => setIsDrawerOpen(false)}
-        >
+        <Box style={{ width: 250 }} onClick={() => setIsDrawerOpen(false)}>
           <List>
             <ListItem button onClick={() => navigate('/')}>
-              <ListItemIcon><LineStyle/></ListItemIcon>
+              <ListItemIcon>
+                <LineStyle />
+              </ListItemIcon>
               <ListItemText>Home</ListItemText>
             </ListItem>
             <ListItem button onClick={() => navigate('/profilebuilder')}>
-              <ListItemIcon><Person/></ListItemIcon>
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
               <ListItemText>Profile Builder</ListItemText>
             </ListItem>
             <ListItem button onClick={logout}>
-              <ListItemIcon><Logout/></ListItemIcon>
+              <ListItemIcon>
+                <Logout />
+              </ListItemIcon>
               <ListItemText>Sign Out</ListItemText>
             </ListItem>
           </List>
@@ -49,4 +62,3 @@ export const Sidebar = () => {
     </React.Fragment>
   );
 };
-
